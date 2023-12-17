69f3115d57b1ea8144770a76211c9375bf571833
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-12-17 18:47:05 +0100

    Basic structure created and navigation implemented with todos api

---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,7 @@
+export default function RestTodosPage() {
+  return (
+    <div>
+      <h1>Todos Page</h1>
+    </div>
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Sidebar } from "./Sidebar";
+export { TopMenu } from "./TopMenu";
+export { WidgetItem } from "./WidgetItem";
