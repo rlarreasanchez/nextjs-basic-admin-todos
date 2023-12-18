4e12f068983a7c4044f249b16050f005de486f6a
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-12-18 11:36:22 +0100

    Playing with new hook useOptimistic from react

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import prisma from "@/lib/prisma";
 import { TodosGrid } from "@/todos";
 import { NewTodo } from "../../../todos/components/NewTodo";
